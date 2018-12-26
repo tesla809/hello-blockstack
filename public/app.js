942ac4c9078f9a834a5e1188f27d29989a9bc43a
anthony.albertorio <anthony.albertorio@gmail.com>
2018-12-25 21:41:36 -0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,33 @@
+// adds eventListeners to selected DOM elements
+// when to initial DOM loaded.
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('signin-button').addEventListener('click', function(event) {
+    event.preventDefault()
+    blockstack.redirectToSignIn()
+  })
+  document.getElementById('signout-button').addEventListener('click', function(event) {
+    event.preventDefault()
+    blockstack.signUserOut(window.location.href)
+  })
+
+  // handler to show profile
+  function showProfile(profile) {
+    var person = new blockstack.Person(profile)
+    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
+    if(person.avatarUrl()) {
+      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
+    }
+    document.getElementById('section-1').style.display = 'none'
+    document.getElementById('section-2').style.display = 'block'
+  }
+
+  // if signed in get userData and pass to handler above
+  if (blockstack.isUserSignedIn()) {
+    var profile = blockstack.loadUserData().profile
+      showProfile(profile)
+  } else if (blockstack.isSignInPending()) {
+    blockstack.handlePendingSignIn().then(function(userData) {
+      window.location = window.location.origin
+    })
+  }
+})
